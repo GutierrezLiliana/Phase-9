305cf54744c9d6df9c74b013660d36270c69fdbe
GutierrezLiliana <liliana_gutierrez@davincischools.org>
2019-03-14 12:31:57 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,43 +8,45 @@ g = document.getElementById("Nars2");
 function purple(el){
 	if (el.src.match("images/highend1.png")){
 		playSound;
-		// if the image is a cherry, change it to a lemon!
+		// if the image is highend1, it plays the ding correct sound.
 	} else if (el.src.match("images/bare.png")){
 		playSound2;
-		// if the image is a lemon, change it to an orange!
+		// if the image is bare, it plays the buzzing wrong sound.
 	} else if (el.src.match("images/nyx-liquid.png")){
 		playSound2;
-		// if the image is an orange, change it to a cherry!
+		// if the image is nyx-liquid, it plays the buzzing wrong sound.
 	}  else if (el.src.match("images/fentygloss.png")){
 		playSound;
-		// if the image is a lemon, change it to an orange!
+		// if the image is fentygloss, it plays the ding correct sound.
 	}
 }
 
 function playSound(el) {
 	document.getElementById('Correct Answer').play();
 	el.classList.add("opaque");
-	// when the JACKPOT! button is clicked, play the slot machine sound
+	// when the correct image is clicked it will turn opaque showing the user that the image is correct while also playing the ding sound.
 }
 function playSound2() {
 	document.getElementById('Wrong Answer').play();
-	// when the JACKPOT! button is clicked, play the slot machine sound
+	// when the wrong image is clicked it will just play the buzzing incorrect sound showing the user that the photo is wrong.
 }
 function checkColors(elem){
 	if (a.classList.contains("opaque") && b.classList.contains("opaque") && c.classList.contains("opaque") && d.classList.contains("opaque")){
 		alert("1st Question Answers are correct! Now you can go onto the second question.");
+	//when all the images for the first question is opaque, it will show a message telling the user that they can go onto the second question.
 	}
 }
 
 function checkColors2(elem){
 	if (e.classList.contains("opaque") && f.classList.contains("opaque") && g.classList.contains("opaque")){
-		alert("2nd Questions Answers are correct! You have completed this escape room and now you can go to the next escape room by clicked on the button.");
+		alert("2nd Questions Answers are correct! You have completed this escape room and now you can go to the next escape room by clicking on the key.");
 			document.getElementById("key").classList.add("show");
 			document.getElementById("key").classList.remove("hide");
-//i wish this function would work when all lights are yellow, or all are red...
+			document.getElementById("key").removeAttribute("checkColors2");
+//when all the images for the second question is opaque, it will show the key and remove the hide class assigned to the image.
 	}
 }
-
+// this is the timer code that is being used to time the user for how long they have to complete the escape room.
 var timeLeft = 60;
     var elem = document.getElementById('some_div');
     
